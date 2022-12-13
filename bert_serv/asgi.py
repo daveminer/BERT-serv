968862726f3f7be40acee7c82d58fa285116647c
Dave Miner <dave.miner@live.com>
2022-12-13 01:23:09 -0500

    django hello world

---
+++ bert_serv/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bert_serv project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bert_serv.settings')
+
+application = get_asgi_application()
---
+++ finbert/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FinbertConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'finbert'
