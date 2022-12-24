ac1fb36106ff4f40d601851c3ec7fb89aceeac61
Dave Miner <dave.miner@live.com>
2022-12-24 09:11:30 -0500

    remove obsolete test

--- sentiment/test_models.py
+++ sentiment/test_models.py
@@ -1,20 +1,19 @@
 import pytest
 from .models import Sentiment
-from torch import Tensor
 
 
-@pytest.mark.django_db(True)
-@pytest.fixture
-def test_success(mocker):
-    mocker.patch('sentiment.models.Sentiment.finbert',
-                 return_value=Tensor([[3.7920, -4.8535, -2.6651]]))
+# @pytest.mark.django_db(True)
+# @pytest.fixture
+# def test_success(mocker):
+#     mocker.patch('sentiment.models.Sentiment.finbert',
+#                  return_value=Tensor([[3.7920, -4.8535, -2.6651]]))
 
-    sentiments_before = __count_sentiments()
+#     sentiments_before = __count_sentiments()
 
-    Sentiment.run(sentences=['Test sentiment sentence.'])
+#     Sentiment.run(sentences=['Test sentiment sentence.'])
 
-    assert __count_sentiments() == sentiments_before + 1
+#     assert __count_sentiments() == sentiments_before + 1
 
 
-def __count_sentiments():
-    return Sentiment.objects.all().count()
+# def __count_sentiments():
+#     return Sentiment.objects.all().count()
