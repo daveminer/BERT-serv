0175d096e9f9d6331c82816d38ce0a0ab89f8695
Dave Miner <dave.miner@live.com>
2022-12-25 22:40:39 -0500

    start test for send webhook

--- sentiment/test_tasks.py
+++ sentiment/test_tasks.py
@@ -1,10 +1,9 @@
 import json
 import numpy
 import pytest
-from .tasks import run_sentiment
+from .tasks import run_sentiment, send_webhook
 
 @pytest.mark.django_db(True)
-@pytest.mark.filterwarnings("ignore:entry_points")
 def test_run_sentiment(mocker):
     mock_finbert = mocker.patch('sentiment.tasks.finbert')
 
@@ -18,3 +17,9 @@ def test_run_sentiment(mocker):
     ).apply().get()
 
     assert mock_results == json.dumps({'ids': [1, 2]})
+
+def test_send_webhook(mocker):
+    mock_post = mocker.patch('sentiment.tasks.requests.post')
+    send_webhook.s({'test': 'value'}, "http://test--url.com").apply()
+
+    mock_post.assert_called_with("http://test--url.com", json={'test': 'value'})
