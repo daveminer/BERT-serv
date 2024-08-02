fc232e33b4756e51b79e4cd250bd402436fffa60
daveminer <wtrispsn@gmail.com>
2024-08-02 08:24:23 -0400

    add filter and pagination params to sentiment index endpoint

--- bert_serv/urls.py
+++ bert_serv/urls.py
@@ -3,17 +3,17 @@ from django.urls import include, path
 from sentiment import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('callback/', include([
-        path('sentiment/', include([
-            path('new/', views.SentimentCallback.as_view()),
+    path('admin', admin.site.urls),
+    path('callback', include([
+        path('sentiment', include([
+            path('new', views.SentimentCallback.as_view()),
         ])),
     ])),
-    path('sentiment/', include([
+    path('sentiment', include([
         path('', views.SentimentList.as_view()),
-        path('<int:pk>/', views.SentimentDetail.as_view()),
-        path('new/', views.SentimentCreate.as_view()),
+        path('/<int:pk>', views.SentimentDetail.as_view()),
+        path('new', views.SentimentCreate.as_view()),
     ])),
-    path('tag/', views.SentimentTagList.as_view())
+    path('tag', views.SentimentTagList.as_view())
 ]
 ()
