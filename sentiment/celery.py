d7bb6ee383235fc22b6dc15711c00b21b153b84d
Dave Miner <wtrispsn@gmail.com>
2025-05-13 13:33:59 -0400

    remove unused otel imports

--- sentiment/celery.py
+++ sentiment/celery.py
@@ -3,10 +3,6 @@ import os
 import logging
 from celery import Celery
 from pathlib import Path
-from opentelemetry._logs import set_logger_provider, get_logger_provider
-from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.semconv.resource import ResourceAttributes
 
